e9531611edf8ad73efe4afbc734d56cc7285d0d0
Grzegorz Sienkiewicz <grzegorz.sienkiewicz95@gmail.com>
2022-09-03 13:21:01 +0200

    Add basic components

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Button } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <View>
+        <Text>Hi, there!</Text>
+      </View>
       <Text>Hello World!</Text>
-      <StatusBar style="auto" />
+      <Button title="click" />
     </View>
   );
 }
